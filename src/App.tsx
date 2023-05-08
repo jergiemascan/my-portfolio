b092de6c52425386c22ff85c864646e276d3a107
Jergie Mascan <jiji.mascan@gmail.com>
2023-05-08 11:14:17 +0200

    added routes and route to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css"
+import Home from "./components/Home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </Router>
+    
   )
 }
 
