c8be80cf9e05662c6b19ea1f255f69e4271b62c7
Jergie Mascan <jiji.mascan@gmail.com>
2024-01-20 17:40:39 +0100

    Updates

--- app-express/model/projectModel.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-export const ProjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  url: { type: String, required: true },
-  desc: { type: String, required: true },
-})
